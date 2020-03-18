32f4212a51d48773d19ae2f58f42fb0fd464529c
khushbu.mobio <khushbus.mobio@gmail.com>
2020-03-18 16:44:54 +0530

    push ssr

---
+++ src/app/public-page/public-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+@Component({
+  selector: 'app-public-page',
+  templateUrl: './public-page.component.html',
+  styleUrls: ['./public-page.component.scss']
+})
+export class PublicPageComponent implements OnInit {
+
+ renderer: string;
+
+  constructor(@Inject(PLATFORM_ID) platformId:any) { 
+    this.renderer = isPlatformBrowser(platformId) ? "Browser" : "serve";
+    console.log(isPlatformBrowser(platformId));
+  }
+
+  ngOnInit() {
+  }
+
+}
